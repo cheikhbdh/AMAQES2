0e78fcc35c1d6d9152a701721a1237e5164efbb3
cheikhbdh <22003@supnum.mr>
2024-06-14 22:37:32 +0000

    boudihethgf

--- example-app/config/app.php
+++ example-app/config/app.php
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://amaqes.up.railway.app'),
 
     'asset_url' => env('ASSET_URL', '/'),
 
